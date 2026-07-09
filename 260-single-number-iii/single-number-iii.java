fcf44a71cf64e351d449b6d117d95c73ccd57770
Chandan Kishor M N <189505651+ChandanKishorMN@users.noreply.github.com>
2026-07-09 20:12:19 +0530

    Time: 1 ms (100.00%) | Memory: 48.2 MB (23.06%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+
+        // XOR of all numbers
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        // Rightmost set bit
+        int diff = xor & (-xor);
+
+        int a = 0, b = 0;
+
+        // Divide into two groups
+        for (int num : nums) {
+            if ((num & diff) == 0) {
+                a ^= num;
+            } else {
+                b ^= num;
+            }
+        }
+
+        return new int[]{a, b};
+    }
+}
